f3fc6f7d706456b417e6b0dbbe062f14cbad7629
Aleksandr Groznykh <groznykh.aleksandr@gmail.com>
2017-03-04 22:06:38 +0400

    upload wtfisthis_bot

---
+++ wtfbot.py
@@ -0,0 +1,40 @@
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext.filters import Filters
+
+import time
+
+
+class Flags:
+    model_dir = "."
+
+FLAGS = Flags()
+from classify import run_inference_on_image
+from conf import TOKEN
+
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+
+def handle(bot, update):
+    # import ipdb; ipdb.set_trace()
+    f = bot.getFile(update.message.photo[0].file_id)
+    fn = "files/{}.jpg".format(time.time())
+    f.download(fn)
+    guess = run_inference_on_image(fn) 
+    bot.sendMessage(chat_id=update.message.chat_id, text="I think it is a {}".format(guess))
+
+handler = MessageHandler(Filters.photo, handle)
+dispatcher.add_handler(handler)
+
+def main():
+    updater.start_polling()
+    # updater.idle()
+
+
+if __name__ == "__main__":
+    # print(run_inference_on_image("/home/suh/Downloads/stakan.jpg"))
+    main()
