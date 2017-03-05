8b16d53cbc52e20b4386b6c5551294e43b6f7e0b
ayratzay <qweqwe1>
2017-03-05 15:30:55 +0300

    adding pic 2 class mapping

---
+++ sasha2vec.py
@@ -0,0 +1,16 @@
+def mapping(rep):
+    for k, v in smapper.items():
+        if rep in v:
+            return k
+    return '009'
+
+smapper = {
+'001': ['plastic bag'],
+'002': ['plastic bin'],
+'003': ['CD DVD'],
+'004': ['magazine'],
+'005': ['toilet paper'],
+'006': ['egg package'],
+'007': ['paper package'],
+'008': ['plastic container']
+}
--- wtfbot1.py
+++ wtfbot1.py
@@ -2,7 +2,7 @@ import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram.ext.filters import Filters
-
+from sasha2vec import mapping
 import time
 
 
@@ -11,20 +11,25 @@ class Flags:
 
 FLAGS = Flags()
 from classify import run_inference_on_image
-from conf import TOKEN
+from config import TOKEN
 
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
+def get_describe_object_by_id(classid==None):
+        if classid == None:
+            return "Please, make more photo and "
+    pass
 
 def handle(bot, update):
     # import ipdb; ipdb.set_trace()
     f = bot.getFile(update.message.photo[-1].file_id)
     fn = "files/{}.jpg".format(time.time())
     f.download(fn)
-    guess = run_inference_on_image(fn) 
+    guess = run_inference_on_image(fn)
+    guessed_class = mapping(guess)
     bot.sendMessage(chat_id=update.message.chat_id, text="I think it is a {}".format(guess))
 
 handler = MessageHandler(Filters.photo, handle)
