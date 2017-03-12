e78b503555ef1e8f9a519f941c0a534bef0035e2
Andrei Demakov <mail.starforge@gmail.com>
2017-03-12 12:59:58 +0300

    Add files via upload
    add map and img

---
+++ image_modyfier.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+from random import randrange
+
+from PIL import Image, ImageFilter
+
+def modify_image(image):
+    #Read image
+    im = Image.open(image).convert("RGBA")
+    #Display image
+    # im.show()
+
+    for k in range(2, 358, 50):
+        im_rotate = im.rotate(k, expand = True)
+        im_rotate_name = image[:image.rindex('.')]+"_rotated_"+k.__str__()+".gif"
+        # transparency = im_rotate.info['transparency']
+        im_rotate.save(im_rotate_name) #, transparency = transparency)
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dir", required=True, help="Директория поиска изображений")
+    args = vars(ap.parse_args())
+
+    directory = args["dir"]
+    #Получаем список файлов в переменную files
+    files = os.listdir(directory)
+    #Фильтруем список
+    images = filter(lambda x: x.endswith('.jpg' or '.gif'), files)
+
+    for i in images:
+        print(i)
+        modify_image(directory+"\\"+i)
